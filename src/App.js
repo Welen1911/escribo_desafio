58f3121051d81a25dc709220340a66cef249d03d
Welen1911 <welenalmeida2210@gmail.com>
2023-11-27 19:18:46 -0300

    Feat: styled components

--- src/App.js
+++ src/App.js
@@ -22,23 +22,34 @@ function App() {
 
   return (
     <>
-      <div>
-        Digite um número: <input type='number' onChange={(e) => setNum(e.target.value)} placeholder='ex: 1' />
-        <button onClick={() => calcular()}>Calcular</button>
-      </div>
-      {lista.length > 0 ?
-        <div>
-          Valores:
-          <ul>{lista.map((valor) => (
-            <li key={valor}>{valor}</li>
-          ))}</ul>
+      <div class="max-w-sm mx-auto mt-5">
+        <div class="mb-5">
+          <label for="number" class="block mb-2 text-sm font-medium text-gray-900 ">Digite um número:</label>
+          <input type="number" onChange={(e) => setNum(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="ex: 1" required />
         </div>
-      : ""}
-      <div>
-        {total > 0 ? "Total: " + total : ""}
-      </div>
-      
-     
+        <div>
+          <button onClick={() => calcular()} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Calcular</button>
+        </div>
+        <div class="mb-5 mt-5">
+          <div>
+            {total > 0 ? "Total: " + total : ""}
+
+          </div>
+        </div>
+        <div class="mb-5">
+          {lista.length > 0 ?
+            <div>
+              Valores:
+              <ul>
+                <li>{lista.map((valor) =>
+                  valor + "; "
+                )}</li>
+              </ul>
+            </div>
+            : ""}
+        </div>
+      </div >
+
     </>
   );
 }
